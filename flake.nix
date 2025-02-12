e2a2b1f29ef15ff1becf28bf123121fbfb470a59
benny@MGC-DRW-CTR01 <ben.r.harris@protonmail.com>
2025-02-12 16:36:47 +0930

    removing flake-parts

--- flake.nix
+++ flake.nix
@@ -2,33 +2,18 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nixvim.url = "github:nix-community/nixvim";
-    flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
-  outputs = {
-    nixvim,
-    flake-parts,
-    ...
-  } @ inputs:
-    flake-parts.lib.mkFlake {inherit inputs;} {
-      systems = [
-        "x86_64-linux"
-        "aarch64-linux"
-      ];
-
-      perSystem = {
-        pkgs,
-        system,
-        ...
-      }: let
-        nixvimPkgs = nixvim.legacyPackages.${system};
-        nixvimModule = {
-          inherit pkgs;
-          module = import ./config.nix;
-        };
-        nvim = nixvimPkgs.makeNixvimWithModule nixvimModule;
-      in {
-        packages.default = nvim;
-      };
+  outputs = {nixpkgs, nixvim, ...}: let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+    nvim = nixvimPkgs.makeNixvimWithModule nixvimModule;
+    nixvimPkgs = nixvim.legacyPackages.${system};
+    nixvimModule = {
+      inherit pkgs;
+      module = import ./config.nix;
     };
+  in {
+    packages.${system}.default = nvim;
+  };
 }
